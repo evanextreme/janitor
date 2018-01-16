b6344fd623970cff6ce8c2806829c2bc545cf99e
Evan Hirsh <evanx11@gmail.com>
2018-01-16 10:30:12 -0500

    macos notification support

--- screenshot.sh
+++ screenshot.sh
@@ -5,11 +5,21 @@ IFS=$'\n'       # make newlines the only separator
 cd ~/Desktop
 export FILES="$(ls)"
 
+FOLDER='~/Doccuments/screenshots'
+FOUND=0
+
 for f in $FILES
 do
 	echo "hit $f"
     if [[ $f == *"Screen Shot"* ]]; then
         echo "moving $f"
-        mv $f ./screenshots
+        mv $f $FOLDER
+        let "FOUND += 1"
     fi
 done
+
+if [[ $FOUND -gt 0 ]] 
+then
+    osascript -e 'display notification "'$FOUND' screenshots archived" with title "evanextreme/janitor"'
+fi
+
