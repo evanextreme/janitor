c0a96427ca1dbbb9bb86f97f91d9bd4d2390694f
Evan Hirsh <4483135+evanextreme@users.noreply.github.com>
2018-09-07 15:57:15 -0400

    move screenshots to stack, move screenshots older than 3 days to trash

--- screenshot.sh
+++ screenshot.sh
@@ -2,22 +2,36 @@
 
 IFS=$'\n'       # make newlines the only separator
 
-cd ~/Desktop
-export FILES="$(ls)"
+export DESKTOP="$(find ~/Desktop -name "Screen Shot*.png")"
 
-FOLDER='/Users/evan/Documents/screenshots'
+SCREENSHOT_PATH='/Users/evan/Documents/Screenshots'
 FOUND=0
 
-for f in $FILES
+for f in $DESKTOP
 do
     if [[ $f == *"Screen Shot"* ]]; then
-        mv $f $FOLDER
+        mv $f $SCREENSHOT_PATH
         let "FOUND += 1"
     fi
 done
 
+export SCREENSHOTS="$(find $SCREENSHOT_PATH -name "Screen Shot*.png" -mmin +4320)"
+
+TRASH_PATH='/Users/evan/.Trash'
+TRASH=0
+
+for f in $SCREENSHOTS
+do
+    if [[ $f == *"Screen Shot"* ]]; then
+        mv $f $TRASH_PATH
+        let "TRASH += 1"
+    fi
+done
+
+
+
 if [[ $FOUND -gt 0 ]] 
 then
-    osascript -e 'display notification "to folder '$FOLDER'" with title "evanextreme/janitor" subtitle "'$FOUND' screenshots archived"'
+    osascript -e 'display notification "added to the Screenshot stack" with title "evanextreme/janitor" subtitle "'$FOUND' screenshots archived"'
 fi
 
